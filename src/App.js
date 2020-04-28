742ffb99b5b20ce9469fb538d7e981b18462173b
Paweł Olbrycht <pawel.olbrycht.pl@gmail.com>
2020-04-28 11:06:10 +0200

    another small update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-      return React.createElement('div', {className: 'App'},
-          React.createElement('h1', null, 'does this work now?'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+          <Person/>
+      </div>
+    );
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work now?'));
   }
 }
 
